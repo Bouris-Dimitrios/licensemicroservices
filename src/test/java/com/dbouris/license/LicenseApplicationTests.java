53b6f664b6036aa6429b87aeab1f9def606fad2d
bourisdim <bourisdim@outlook.com>
2023-04-02 09:09:04 +0300

    Initial commit

---
+++ src/test/java/com/dbouris/license/LicenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbouris.license;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
